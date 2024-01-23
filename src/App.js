48d914ff35ce79ac8181671cf58c00501c427842
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-23 10:36:33 +0000

    Added a footer with the Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Mariadoc1" target="_blank" rel="noreferrer">
+          Maria Docherty
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Mariadoc1/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
