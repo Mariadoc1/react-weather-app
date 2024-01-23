d7b6482839acd2ed8b2f113eba2a775a5c577789
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-23 12:05:05 +0000

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,55 @@
 import React from "react";
-
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Glasgow</h1>
+      <ul>
+        <li>Tuesday 11:00</li>
+        <li>Rain</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+              alt="Rain"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 100%</li>
+            <li>Humidity: 88%</li>
+            <li>Wind: 14 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
